1c465f02d7ac7215314eef910695e56f1531d077
HisenLee <Hisen.lee.iverson@gmail.com>
2019-03-04 09:17:29 +0800

    first commit

---
+++ J2SETestWithMaven/src/main/java/com/testj2se/App.java
@@ -0,0 +1,18 @@
+package com.testj2se;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println( "Hello World!" );
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
